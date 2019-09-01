35bb0595edaede330dcba0de15ba2215b3a86b67
rdull <rdull@rdull-pc.wireless-1x.unc.edu>
2019-09-01 19:59:13 -0400

    2 decimal places

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,19 +10,26 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Takes entered value of number of customers
 		int customerCount = scan.nextInt();
+		// Uses scan to retrieve a first and last name and total number of groceries
 		for (int i = 0; i < customerCount ; i++) {
 			String firstName = scan.next();
 			String lastName = scan.next();
 			int itemCount = scan.nextInt();
 			double total = 0.0;
+			// Establishes each individual item's total price by multiplying 
+			// quantity by price
 			for (int j = 0; j < itemCount; j++) {
 				int quantity = scan.nextInt();
 				String itemName = scan.next();
 				double price = scan.nextDouble();
 				total += quantity * price;
+				
 			}
-			System.out.println(firstName.charAt(0) + ". " + lastName + ": " + total);
+			// Prints each customer's total cost
+			System.out.println(firstName.charAt(0) + ". " + lastName + ": " 
+			+ String.format("%.2f", total));
 		}
 		
 		
