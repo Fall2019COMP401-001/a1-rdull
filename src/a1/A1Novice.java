d71b03ce0e3a631ebffedcc704a365bb6efc55b5
rdull <rdull@rdull-pc.wireless-1x.unc.edu>
2019-09-01 16:30:12 -0400

    Wrong Syntax but first attempt at solving

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,38 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Class for items
+		class Item {
+			String itemName = scan.next();
+			int quantity = scan.nextInt();
+			double price = scan.nextDouble();			
+		}
+		
+		// Class for customers
+		class Customer {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int count = scan.nextInt();
+			Item[] items = new Item[count];
+			
+			for (i = 0, i < count, i++) {
+				Item[0] = new Item();
+			}
+		}
+		
+		// receive customer count
+		int customerCount = scan.nextInt();
+		
+		// creates correct number of customers and those customers
+		for (i = 0; i < customerCount; i++) {
+			Customer j = new Customer();
+			double total = 0.0;
+			int countItems = j.items.count();
+			// calculates total price for the customer
+			for (x = 0; x < countItems; x++) {
+				total += j.items[x].quantity * j.items[x].price;
+			}
+			System.out.println(j.firstName[0] + ". " + j.lastName + ": "+ total);	 
+		}
 	}
-}
+} 
