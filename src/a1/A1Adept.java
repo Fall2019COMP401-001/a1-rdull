76271ef2961f7521be35e0433044bc84b7818af4
rdull <rdull@rdull-pc.wireless-1x.unc.edu>
2019-09-01 23:46:03 -0400

    Finally got it working with .equals()

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,64 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int storeItemCount = scan.nextInt();
+		String[] iNames = new String[storeItemCount];
+		double[] iPrices = new double[storeItemCount];
 		
+		for (int i = 0; i < storeItemCount; i++) {
+			iNames[i] = scan.next();
+			iPrices[i] = scan.nextDouble();
+		}
+		
+		int customerCount = scan.nextInt();
+		double total = 0.0;
+		double biggestNumber= 0;
+		String biggestFirst = "";
+		String biggestLast = "";
+		double smallestNumber= 10000;
+		String smallestFirst = "";
+		String smallestLast = "";
+		
+		for (int i = 0; i < customerCount ; i++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int itemCount = scan.nextInt();
+			double customerTotal = 0.0;
+			
+			for (int j = 0; j < itemCount; j++) {
+				int quantity = scan.nextInt();
+				String itemName = scan.next();
+				double price = 0.0;
+				
+				for (int k = 0; k < storeItemCount; k++) {
+					
+					if (itemName.equals(iNames[k])) {
+						price = iPrices[k];
+						double cost = quantity * price;
+						total += quantity * price;
+						customerTotal += cost;
+					}
+				}
+			}
+			
+			if (customerTotal > biggestNumber) {
+				biggestNumber = customerTotal;
+				biggestFirst = firstName;
+				biggestLast = lastName;
+			}
+			
+			if (customerTotal < smallestNumber) {
+				smallestNumber = customerTotal;
+				smallestFirst = firstName;
+				smallestLast = lastName;
+			}
+		}
+		
+		double average = total / customerCount;
+		System.out.println("Biggest: " + biggestFirst + " " + biggestLast + " (" 
+				+ String.format("%.2f", biggestNumber) + ")");
+		System.out.println("Smallest: " + smallestFirst + " " + smallestLast + " (" 
+				+ String.format("%.2f", smallestNumber) + ")");
+		System.out.println("Average: " + String.format("%.2f", average));
 	}
 }
